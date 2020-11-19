3810badcbb2c61c0f78c1e2d13a57aff2c1907fa
Pavan <pavangarapati1987@gmail.com>
2020-11-19 09:45:00 +0530

    Updated Code`:wq

--- New folder/fcd/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-
-
-# Create your views here.
-def login(request)
-	form = AuthenticationForm()
-	return render(request, 'fcd/login.html', {'form':form})
--- New folder/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path('', include('fcdtest.urls')),
-    path('', views.login, name='login'),
-]
