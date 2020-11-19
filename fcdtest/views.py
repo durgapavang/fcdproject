3043b233cb2e3169875cff325bb392516f5801ef
Pavan <pavangarapati1987@gmail.com>
2020-11-19 12:10:14 +0530

    Updated Code

--- fcdtest/views.py
+++ fcdtest/views.py
@@ -2,17 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import mysql.connector
 from operator import itemgetter 
-# from .models import User
+from .models import User
 
 # Create your views here.
 def login(request):
 	if request.method == 'POST':
-		# user = User()
-		# user.email = request.POST['email']
-		# user.password = request.POST['password']
-		print("dfds")
+		user = User()
+		user.email = request.POST['email']
+		user.password = request.POST['password']
+		print(user)
+		return render(request, 'dashboard.html')
 	else:
 		return render(request, 'login.html')
 	
 def dashboard(request):
-	return render(request, 'dashboard.html')
+	if request.method == 'POST':
+		return render(request, 'dashboard.html')
+	else:
+		return render(request, 'dashboard.html')
